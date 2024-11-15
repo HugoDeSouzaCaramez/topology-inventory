14ad98a86d09e7b1c2513ee75cb6c333d2a71e20
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-15 14:41:01 -0300

    Implementando demais classes de especificação de domínio

---
+++ domain/src/main/java/dev/hugodesouzacaramez/topologyinventory/domain/specification/NetworkAmountSpec.java
@@ -0,0 +1,21 @@
+package dev.hugodesouzacaramez.topologyinventory.domain.specification;
+
+import dev.hugodesouzacaramez.topologyinventory.domain.entity.Equipment;
+import dev.hugodesouzacaramez.topologyinventory.domain.entity.Switch;
+
+public final class NetworkAmountSpec extends AbstractSpecification<Equipment> {
+
+    final static public int MAXIMUM_ALLOWED_NETWORKS = 6;
+
+    @Override
+    public boolean isSatisfiedBy(Equipment switchNetwork) {
+        return ((Switch)switchNetwork).getSwitchNetworks().size()
+                <=MAXIMUM_ALLOWED_NETWORKS;
+    }
+
+    @Override
+    public void check(Equipment equipment) throws GenericSpecificationException {
+        if(!isSatisfiedBy(equipment))
+            throw new GenericSpecificationException("The max number of networks is "+ NetworkAmountSpec.MAXIMUM_ALLOWED_NETWORKS);
+    }
+}
