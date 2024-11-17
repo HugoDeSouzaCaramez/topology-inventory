67cefaca2a159fa08967dc374ddd1fc5dd30c197
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-17 02:13:03 -0300

    Conseguindo executar testes, inclusive em ApplicationTest

--- application/src/test/java/dev/hugodesouzacaramez/topologyinventory/application/ApplicationTest.java
+++ application/src/test/java/dev/hugodesouzacaramez/topologyinventory/application/ApplicationTest.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "html:target/cucumber-result"}
+        features = "src/test/resources/dev/hugodesouzacaramez/topologyinventory/application", // Diretório raiz das features
+        glue = "dev.hugodesouzacaramez.topologyinventory.application", // Pacote com os steps
+        plugin = {"pretty", "html:target/cucumber-reports.html"} // Relatórios opcionais
 )
 public class ApplicationTest {
-
 }
