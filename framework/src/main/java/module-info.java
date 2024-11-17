88caeb922356d03ff1b1470b647573fe4750e8d9
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-17 20:08:33 -0300

    corrigindo erros de compilação e realizando testes com sucesso

--- framework/src/main/java/module-info.java
+++ framework/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module framework {
     requires domain;
     requires application;
-    requires jakarta.persistence;
     requires static lombok;
     requires org.eclipse.persistence.core;
+    requires java.sql;
+    requires jakarta.persistence;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.core;
+
+    exports dev.hugodesouzacaramez.topologyinventory.framework.adapters.output.h2.data;
+    opens dev.hugodesouzacaramez.topologyinventory.framework.adapters.output.h2.data;
 }
